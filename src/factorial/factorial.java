1579e24ca9b72cb5e06ca58b5b2ae2b1a3764881
Philip <robin.philip@nokia.com>
2021-05-23 17:28:03 +0530

    fact commit

---
+++ src/factorial/factorial.java
@@ -0,0 +1,27 @@
+package factorial;
+
+public class factorial {
+
+public static void main(String args[])
+{
+    int i,fact=1;
+    int number=5;
+    for (i=1;i<=number;i++)
+    {
+        fact=fact*i;
+    }
+System.out.println("factorial is "+fact);
+    int result=fact1(number);
+    System.out.println("factorial is "+fact1(number));
+}
+static int fact1(int n){
+    if(n==0){
+        return 1;
+    }
+    else {
+         return(n*fact1(n-1));
+
+    }
+
+}
+}
